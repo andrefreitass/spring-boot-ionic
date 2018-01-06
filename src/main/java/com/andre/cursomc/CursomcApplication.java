597dcb7a82ce3c2f52ecffde1dfd64418845ac97
Andre Freitas <andrefreitass7@gmail.com>
2018-01-05 22:35:56 -0200

    Criando operacao de Instaciacao, ao subir o ambiente ja e inserido os dados na tabela categoria

--- src/main/java/com/andre/cursomc/CursomcApplication.java
+++ src/main/java/com/andre/cursomc/CursomcApplication.java
@@ -1,12 +1,34 @@
 package com.andre.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.andre.cursomc.domain.Categoria;
+import com.andre.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
+		
+		
+	}
+	
+	
 }
