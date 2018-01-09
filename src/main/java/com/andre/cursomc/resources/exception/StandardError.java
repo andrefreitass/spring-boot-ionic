e9db14a2c51291f57deccfed4307200a4110404e
Andre Freitas <andrefreitass7@gmail.com>
2018-01-08 23:29:40 -0200

    Personalizando o retorno da validacao, criacao de lista/tratamento de erros

--- src/main/java/com/andre/cursomc/resources/exception/StandardError.java
+++ src/main/java/com/andre/cursomc/resources/exception/StandardError.java
@@ -2,12 +2,16 @@ package com.andre.cursomc.resources.exception;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class StandardError implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
 	private Integer status;
-	private String msg;
+	private String msg;	
+	
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Long timeStamp;
 	
 	public StandardError(Integer status, String msg, Long timeStamp) {
