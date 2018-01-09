dbd5efaa09666acb2309b3ce33640bd03dcc72b8
Andre Freitas <andrefreitass7@gmail.com>
2018-01-09 09:43:13 -0200

    Ajustes de projeto

--- src/main/java/com/andre/cursomc/domain/Cliente.java
+++ src/main/java/com/andre/cursomc/domain/Cliente.java
@@ -15,8 +15,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import org.hibernate.annotations.Cascade;
-
 import com.andre.cursomc.domain.enums.TipoCliente;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
